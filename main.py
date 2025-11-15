868a813b7ed1f30fa0a447916cc0c1668478e844
IS250 Student <changeme@example.com>
2025-11-15 12:48:01 -0600

    Finished pseudocode

--- main.py
+++ main.py
@@ -1,18 +1,60 @@
 """
-Full Name:
-Class-Section:
-Assignment Title:
-Submission Date:
+Full Name: Ryan Serbus
+Class-Section: IS 250-01
+Assignment Title: Calculate Average of Three Scores
+Submission Date: 11/17/2025
 """
 """
-Write your pseudocode here.
-Do not write any Python code in this section.
-Your pseudocode should describe your overall approach in your own words.
+Pseudo Code: 
+Ask the user for three scores
+"What is score 1: "
+"What is score 2: "
+"What is score 3: "
+Print each score to show the user
+"You entered these scores: "
+"score 1: "
+"score 2: "
+"score 3: "
+Find the average of three scores
+"The average score is: "
 """
+#define function for average scores
+def calculate_average(one, two, three):
 
-# Your Python code begins below this line.
-# Every line you write must have a comment directly above it.
+#return the average
+    return (one + two + three) / 3
 
-# Call your function when your program is ready
-calculate_average()   
+#define main
+def main ():
+
+#ask for first score
+    one = float(input("What is score 1: "))
+
+#ask for second score
+    two = float(input("What is score 2: "))
+
+#ask for third score
+    three = float(input("What is score 3: "))
+
+#confirm the scores:
+    print("You have entered these scores:")
+
+#print the first score
+    print("Score 1:",one)
+
+#print the second score
+    print("Score 2:",two)
+
+#print the third score
+    print("Score 3:",three)
+
+#call calculate_average function
+    average = calculate_average(one, two, three)
+
+#print the average score
+    print(f"The average score is: {average:.2f}")
+
+#call main
+main()
+
   
